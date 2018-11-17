41819c9f896a7a73e7982648f43c88c328b6b6a6
e <1808297916@qq.com>
2018-11-18 00:28:15 +0800

    新增一个开源项目

---
+++ Project/识别手写数字程序_C#/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(500, 500);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown_1);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove_1);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp_1);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(567, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(165, 50);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "清除画板";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(528, 94);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(162, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "我猜结果是：";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(803, 123);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 151);
+            this.label2.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.AcceptsReturn = true;
+            this.textBox1.AcceptsTab = true;
+            this.textBox1.AllowDrop = true;
+            this.textBox1.Location = new System.Drawing.Point(786, 99);
+            this.textBox1.MaxLength = 100000;
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(309, 401);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(806, 30);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(58, 38);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "学习";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox2.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.textBox2.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox2.Location = new System.Drawing.Point(870, 30);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(46, 38);
+            this.textBox2.TabIndex = 6;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(923, 49);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(48, 21);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "导出";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(923, 28);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(48, 21);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "导入";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(1005, 27);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(58, 19);
+            this.radioButton1.TabIndex = 10;
+            this.radioButton1.Text = "数据";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Checked = true;
+            this.radioButton2.Location = new System.Drawing.Point(1005, 52);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(58, 19);
+            this.radioButton2.TabIndex = 11;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "预览";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(803, 285);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 180);
+            this.label3.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("宋体", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(541, 134);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(209, 89);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "无答案";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1095, 500);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Project/识别手写数字程序_C#/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1{
+    public partial class Form1 : Form{
+        public Form1(){
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e){
+
+        }
+
+        private Point p1, p2;
+        private static bool drawing = false;
+        int[,] s = new int[10, 10];
+        List<Yangben> yb = new List<Yangben>();
+
+        //开始与样本进行匹配
+        private int calc_solve(){
+            //关键代码
+            int _max = -1, id=-1;
+            double _maxsame = 0;
+            string _c = "";
+            for(int i = 0; i < yb.Count; i++) {
+                double sa = yb[i].Sameas(s);
+                _c += yb[i].val + " : " + sa + "\r\n";
+                if (sa > _maxsame) {
+                    _max = yb[i].val;
+                    _maxsame = sa;
+                    id = i;
+                }
+            }
+            textBox1.Text = _c;
+            return id;
+        }
+        //picture_画板鼠标落下
+        private void pictureBox1_MouseDown_1(object sender, MouseEventArgs e){
+            p1 = new Point(e.X, e.Y);
+            p2 = new Point(e.X, e.Y);
+            drawing = true;
+        }
+        //picture_画板鼠标移动
+        private void pictureBox1_MouseMove_1(object sender, MouseEventArgs e){
+            Graphics g = pictureBox1.CreateGraphics();
+            if (e.Button == MouseButtons.Left){
+                if (drawing){
+                    //drawing = true;
+                    Point currentPoint = new Point(e.X, e.Y);
+                    g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;//消除锯齿
+                    g.DrawLine(new Pen(Color.Red, 2), p2, currentPoint);
+
+                    p2.X = currentPoint.X;
+                    p2.Y = currentPoint.Y;
+
+                    //统计像素
+                    int tx, ty;
+                    tx = e.X / 50;
+                    ty = e.Y / 50;
+                    if (tx >= 10) tx = 9; if (tx < 0) tx = 0;
+                    if (ty >= 10) ty = 9; if (ty < 0) ty = 0;
+                    s[ty,tx] = 1;
+
+                    //记录极值
+                    //uu = math.min(uu, e.y);
+                    //ub = math.max(ub, e.y);
+                    //ul = math.min(ul, e.x);
+                    //ur = math.max(ur, e.y);
+                }
+            }
+        }
+        //picture_画板鼠标抬起
+        private void pictureBox1_MouseUp_1(object sender, MouseEventArgs e){
+            drawing = false;
+
+            //几何拉伸
+            //int[,] ss = new int[10, 10];
+            //int tu = uu / 50, tb = ub / 50, tl = ul / 50, tr = ur / 50;
+            //if (tu >= 10) tu = 9; if (tb >= 10) tb = 9; if (tl >= 10) tl = 9; if (tr >= 10) tr = 9;
+            //int tw = tr - tl, th = tb - tu;
+            //for (int i = tu; i <= tb; i++) {
+            //    for(int j = tl; j <= tr; j++) {
+            //        if(s[(i-tu)*(10/th), (j - tl) * (10 / tw)] == 1) {
+            //            ss[]
+            //        }
+            //    }
+            //}
+
+
+            string sss = "";
+            int c=calc_solve();
+            if (c == -1) sss += "无答案";
+            else sss += yb[c].val;
+            label4.Text = sss;
+
+            string co = "";
+            for(int i = 0; i < 10; i++){
+                for(int j = 0; j < 10; j++){
+                    if (s[i, j] == 1)co += "◼";
+                    else co += "◻";
+                }
+                co += "\n";
+            }
+            label2.Text = co;
+
+            co = "";
+            if(c!=-1)for (int i = 0; i < 10; i++) {
+                for (int j = 0; j < 10; j++) {
+                    if (yb[c].s[i, j] == 1) co += "◼";
+                    else co += "◻";
+                }
+                co += "\n";
+            }
+            label3.Text = co;
+        }
+        //radio_控制台视图
+        private void radioButton1_CheckedChanged(object sender, EventArgs e) {
+            label2.Visible = false;
+            label3.Visible = false;
+            textBox1.Visible = true;
+        }
+        //radio_预览视图
+        private void radioButton2_CheckedChanged(object sender, EventArgs e) {
+            label2.Visible = true;
+            label3.Visible = true;
+            textBox1.Visible = false;
+        }
+        //button_学习按钮
+        private void button2_Click(object sender, EventArgs e){
+            int v = int.Parse(textBox2.Text);
+            yb.Add(new Yangben(v,s));
+
+            button1_Click_1(null,null);
+        }
+        //button_导出配置
+        private void button3_Click(object sender, EventArgs e) {
+            string co = "";
+            for(int i = 0; i < yb.Count; i++) {
+                if (i != 0) co += ",";
+                co += "" + yb[i].val;
+                for(int j = 0; j < 10; j++) {
+                    int m = 0;
+                    for(int k = 0; k < 10; k++) {
+                        if (yb[i].s[j, k] == 1) m |= (1 << k);
+                    }
+                    co += "-"+ m;
+                }
+            }
+            textBox1.Text = co;
+        }
+        //button_导入配置
+        private void button4_Click(object sender, EventArgs e) {
+            string ci = textBox1.Text;
+            string[] fo;
+            fo = ci.Split(',');
+            for(int i = 0; i < fo.Length; i++) {
+                string t = fo[i];
+                string[] fot = t.Split('-');
+                Yangben ybt = new Yangben();
+                ybt.val = int.Parse(fot[0]);
+                for(int j = 0; j < 10; j++) {
+                    int foti = int.Parse(fot[j+1]);
+                    for (int k = 0; k < 10; k++) {
+                        if (((foti >> k) & 1) == 1) ybt.s[j, k] = 1;
+                        else ybt.s[j, k] = 0;
+                    }
+                }
+                yb.Add(ybt);
+            }
+        }
+        //button_清除画板按钮
+        private void button1_Click_1(object sender, EventArgs e){
+            drawing = false;
+
+            Graphics g = pictureBox1.CreateGraphics();
+            g.Clear(Color.White);
+
+            for(int i = 0; i < 10; i++){
+                for(int j = 0; j < 10; j++){
+                    s[i, j] = 0;
+                }
+            }
+        }
+    }
+    //样本类，用于记录样本，以及样本检验方法
+    public class Yangben{
+        public int[,] s = new int[10,10];
+        public int val;
+        public Yangben() { }
+        public Yangben(int v,int[,] arg){
+            s = new int[10, 10];
+            for(int i = 0; i < 10; i++) {
+                for(int j = 0; j < 10; j++) {
+                    s[i, j] = arg[i, j];
+                }
+            }
+            //s = arg;
+            val = v;
+        }
+        public double Sameas(int[,] arg) {
+            double sa = 0, sb = 0 ;
+            for(int i = 0; i < 10; i++) {
+                for(int j = 0; j < 10; j++) {
+                    if (s[i, j] == 1 || arg[i, j] == 1) sb++;
+                    if (s[i, j] == 1 && arg[i, j] == 1) sa++;
+                    else {
+                        int y1 = 0,y2 = 0;
+                        for(int k1 = -1; k1 <= 1; k1++) {
+                            if ((k1 == -1 && i == 0)||(k1 == 1 && i == 9)) continue;
+                            for(int k2 = -1; k2 <= 1; k2++) {
+                                if ((k2 == -1 && j == 0) || (k2 == 1 && j == 9)) continue;
+                                if (s[i + k1, j + k2] == 1) { y1 = 1;}
+                                if (arg[i + k1, j + k2] == 1) { y2 = 1; }
+                            }
+                        }
+                        if (y1 == 1 && y2 == 1) {
+                            sa += 0.3;
+                        }
+                    }
+                }
+            }
+            return 1.0*sa/sb*100;
+        }
+    }
+}
